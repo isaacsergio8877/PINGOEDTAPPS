c0774e37729d379b4472ab71cfd7bc5f75a07c4e
isaacsergio8877 <144507055+isaacsergio8877@users.noreply.github.com>
2023-12-08 01:05:15 -0300

    new

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ menu.onclick = () => {
     navlinks.classList.toggle('open')
 }
 
+function performSearch() {
+    // Obter o valor digitado na barra de pesquisa
+    var searchTerm = document.getElementById('searchInput').value.toLowerCase();
+
+    // Obter todas as divs com a classe "searchable"
+    var searchables = document.querySelectorAll('.searchable');
+
+    // Iterar sobre as divs e ocultar aquelas que não correspondem ao termo de pesquisa
+    searchables.forEach(function (div) {
+        var textContent = div.textContent.toLowerCase();
+        if (textContent.includes(searchTerm)) {
+            // Exibir div se contiver o termo de pesquisa
+            div.classList.remove('hidden');
+        } else {
+            // Ocultar div se não contiver o termo de pesquisa
+            div.classList.add('hidden');
+        }
+    });
+}
